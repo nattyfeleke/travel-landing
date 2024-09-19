477bc9c72fd69832fcf32cb9b722001c76647142
nattyfeleke <nfeleke568@gmail.com>
2024-09-19 16:15:47 +0300

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className="bg-red-500">App</div>
+     
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/routes/__root.tsx
@@ -0,0 +1,18 @@
+import { createRootRoute,  Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <div className="p-2 flex gap-2">
+      
+        {/* <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link> */}
+      </div>
+      <hr />
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  ),
+})
