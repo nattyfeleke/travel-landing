1a6a68b7a0fe84fefd119c11ba1b71735a25fef0
nattyfeleke <nfeleke568@gmail.com>
2024-09-22 15:04:23 +0300

    added tags on destinations

--- src/components/tour/Destinations.tsx
+++ src/components/tour/Destinations.tsx
@@ -8,9 +8,15 @@ interface DestinationsProps {
 }
 const Destinations:React.FC<DestinationsProps> = ({queryString,destinations}) => {
   return (
-    <div>
-        <h4 className='text-sm my-1 text-gray-500'>Destinations</h4>
-       {destinations.length>0 ? destinations.map(destination=> (<div className='hover:bg-gray-300 px-4 cursor-pointer' key={destination.dbid}>{destination.destinationName}</div>)):<div>No result for <span className='text-xs italic'>{queryString}</span></div>}
+<div className=''>
+        <h4 className='text-sm my-1 text-gray-500 px-2 font-bold'>Destinations</h4>
+       {destinations.length>0 ? destinations.map(destination=> (<div className='px-4 font-semibold flex flex-col w-full' key={destination.dbid}>{destination.destinationName} 
+        <div className="flex flex-wrap gap-1 pt-2 border-t my-1">
+
+{destination.tags.length>0 && destination.tags.map(tag=><div className='border rounded-full px-2 py-1 w-fit font-normal text-xs' key={tag.tagId}>{tag.tagName}</div>)}
+        </div>
+
+       </div>)):<div>No result for <span className='text-xs italic'>{queryString}</span></div>}
     </div>
   )
 }
--- src/routes/tour.tsx
+++ src/routes/tour.tsx
@@ -62,22 +62,30 @@ const Tour = ()=>{
         }
       };
 const loading = status==='loading' && task==='fetch-destinations'
-    return(<div className='h-screen flex items-center justify-center'>
+    return(<div className='h-full min-h-screen flex items-center justify-center'>
 
 <form className="max-w-md w-96 mx-auto">
 <h2 className='text-sm font-medium text-gray-900'>Search for Tours</h2>
   <div className="mb-5">
     <label htmlFor="search" className="block mb-2 text-sm font-medium text-gray-600 dark:text-white">Search for a place or activity</label>
-    <input type="text" id="search" className=" bg-gray-50 border border-gray-300 text-gray-600 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={searchQuery} required onChange={handleSearchInput} />
-    <div className="flex flex-col gap-1 py-2  bg-gray-50">
+    <input type="text" id="search" className=" bg-gray-50 border border-gray-300 text-gray-600 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={searchQuery} required onChange={handleSearchInput} placeholder='Eg: Lagos, Nigeria' />
+   
 
-    {loading? <p>Loading ...</p>: searchQuery && <>
+    {loading? <div className='animate-pulse mt-2 flex flex-col gap-1'>
+
+        <div className="h-4 bg-gray-200 rounded-lg" ></div>
+        <div className="h-4 bg-gray-200 rounded-lg" ></div>
+        <div className="h-4 bg-gray-200 rounded-lg" ></div>
+    </div>: searchQuery && <div className='flex flex-col gap-1 py-2  bg-gray-50 mt-2'>
         <Destinations queryString={searchQuery} destinations={destinations}/>
         <Products queryString={searchQuery} products={products}/>
-    </> }
-    </div>
+    </div> }
+    
   </div>
- 
+  <div className="mb-5">
+    <label htmlFor="search" className="block mb-2 text-sm font-medium text-gray-600 dark:text-white">When</label>
+    <input type="date" id="date" className=" bg-gray-50 border border-gray-300 text-gray-600 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required  placeholder='Select dates' />
+    </div>
   <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
 </form>
     </div>)
