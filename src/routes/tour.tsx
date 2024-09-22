f962089b9f4f46ad7547a154b5bb6145eedc4643
nattyfeleke <nfeleke568@gmail.com>
2024-09-22 15:07:40 +0300

    made button full width

--- src/routes/tour.tsx
+++ src/routes/tour.tsx
@@ -86,7 +86,7 @@ const loading = status==='loading' && task==='fetch-destinations'
     <label htmlFor="search" className="block mb-2 text-sm font-medium text-gray-600 dark:text-white">When</label>
     <input type="date" id="date" className=" bg-gray-50 border border-gray-300 text-gray-600 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required  placeholder='Select dates' />
     </div>
-  <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+  <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm !w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search for tours</button>
 </form>
     </div>)
 }
